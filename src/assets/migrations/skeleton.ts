c0b4681a70c1042a1c86abcc42e2ce1565ece1fe
Cristian Torres <cris.torres15@gmail.com>
2026-03-27 12:52:22 -0600

    Add typescript skeleton migration file for Sequelize

---
+++ src/assets/migrations/skeleton.ts
@@ -0,0 +1,21 @@
+import { Migration } from 'sequelize-cli';
+
+export default {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+} as Migration;
